74223727031bce635ef0d04dba0de0da8bf8529c
itggot-adam-ohlsson <adam.olsson@itggot.se>
2017-04-19 11:15:03 +0200

    Börjat på layout!

---
+++ ultimate_navbar/site/js/searchResize.js
@@ -0,0 +1,4 @@
+function searchResize() {
+  document.querySelector("#search_bar").classList.toggle("search_resize");
+  document.querySelector("#search_field").classList.toggle("search_field_resize");
+}
